637500ee2c9070d46a40d6c667f615bbb5c9cea1
Shrishail Patil <91820178+shrishail356@users.noreply.github.com>
2023-10-15 21:46:35 +0530

    uploaded project

---
+++ src/App.js
@@ -0,0 +1,43 @@
+/*import React from 'react';
+import './App.css';
+import ColorPicker from './components/ColorPicker';
+
+function App() {
+  const colors = ["#FF5733", "#33FF68", "#337BFF", "#FFE833", "#FF33E8"];
+
+  return (
+    <div className="App">
+      <ColorPicker colors={colors} />
+    </div>
+  );
+}
+
+export default App;
+*/
+/*import React from 'react';
+import './App.css';
+import UserTable from '../src/components/UserTable';
+
+function App() {
+  return (
+    <div className="App">
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
+*/
+import React from 'react';
+import './App.css';
+import AgeCalculator from '../src/components/AgeCalculator';
+
+function App() {
+  return (
+    <div>
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
